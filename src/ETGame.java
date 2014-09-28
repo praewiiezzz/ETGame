d8effc5fdb28cf9272bff26cb2a8e4e32b8af541
praewiiezzz <modzaasard@hotmail.com>
2014-09-28 15:47:49 +0700

    Create Class Block for create map

--- src/ETGame.java
+++ src/ETGame.java
@@ -8,7 +8,8 @@ public class ETGame extends BasicGame {
 
 	private Map map;
 	private PlayerController player;
-	
+	private Block block;
+
 	public ETGame(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
@@ -17,28 +18,30 @@ public class ETGame extends BasicGame {
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		// TODO Auto-generated method stub
-		map.draw();
+	//	map.draw();
+		block.render();
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		// TODO Auto-generated method stub
-		map = new Map(100,150);
+		//map = new Map(100, 150);
+		block = new Block(100,150);
+		
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
 		// TODO Auto-generated method stub
-		
+
 	}
-	 
-	public static void main(String[] arg)
-	{
+
+	public static void main(String[] arg) {
 		try {
 			ETGame game = new ETGame("E.T. Puzzle");
 			AppGameContainer appgc = new AppGameContainer(game);
-			appgc.setDisplayMode(600, 600, false);
-			appgc.setTargetFrameRate(60);  // FramRate 100 คือ update 100 ครั้งใน 1 วิ 
+			appgc.setDisplayMode(550, 600, false);
+			appgc.setTargetFrameRate(60); // FramRate 100 คือ update 100 ครั้งใน 1 วิ
 			appgc.start();
 		} catch (SlickException e) {
 			e.printStackTrace();
--- src/Map.java
+++ src/Map.java
@@ -1,52 +1,64 @@
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
-
-
 public class Map {
 	private Image floor;
 	private Image BrokenGlass;
 	private int x;
 	private int y;
-	
+	private int nextBlock = 50;
+	public int Box[][] = new int[7][7];
+	public int LocateBox[][] = new int[7][7];
+
 	public Map(int x, int y) throws SlickException {
 		floor = new Image("res/floor.jpg");
 		BrokenGlass = new Image("res/Brokenglass2.jpg");
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void draw() {
 		// TODO Auto-generated method stub
 		int MapX = x;
 		int MapY = y;
-		for (int i = 0 ; i< 7; i++)
-		{
-			MapX = x;
-			for (int j = 0; j< 8 ;j++)
-			{
-				if (MapX%150==0 && MapY%150==0)
-				{
-					BrokenGlass.draw(MapX,MapY);
-				}
-				else {
-				floor.draw(MapX, MapY);
-				}
-				MapX +=50;
-				
-			}
-			MapY +=50;
-		}
+		CreateBox();
+		/*
+		 * for (int i = 0 ; i< 7; i++) { MapX = x; for (int j = 0; j< 7 ;j++) {
+		 * if (MapX%150==0 && MapY%150==0) { BrokenGlass.draw(MapX,MapY); } else
+		 * { floor.draw(MapX, MapY); } MapX +=nextBlock;
+		 * 
+		 * } MapY +=nextBlock; }
+		 */
+
+		
 	}
-	public void draw(int x,int y) {
+
+	public void draw(int x, int y) {
 		// TODO Auto-generated method stub
 		floor.draw(x, y);
 	}
 
 	public void SetMap() {
 		// TODO Auto-generated method stub
-		x = 200;
-		y = 200;
+
+	}
+	
+	public void CreateBox() {
+		int MapX = x;
+		int MapY = y;
+		for (int i = 0; i < 7; i++) {
+			MapX = x;
+			for (int j = 0; j < 7; j++) 
+			{
+				Box[i][j] = 0;
+				//LocateBox[i][j]= LocateBox[MapX][MapY];
+
+				floor.draw(MapX, MapY);
+				MapX += nextBlock;
+
+			}
+			MapY += nextBlock;
+		}
 	}
 
 }
